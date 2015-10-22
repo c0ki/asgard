824ffdabc54d8f79e9d9bb9abc5c4c1b6cad9906
c0ki <cokinou.web@gmail.com>
2015-10-22 23:15:15 +0200

    Delete project parameter to get it from master request

--- src/DashboardBundle/Controller/DefaultController.php
+++ src/DashboardBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($project)
+    public function indexAction()
     {
+        $project = $this->get('request_stack')->getMasterRequest()->attributes->get('_project');
         return $this->render('DashboardBundle:Default:index.html.twig', array('project' => $project));
     }
 }
