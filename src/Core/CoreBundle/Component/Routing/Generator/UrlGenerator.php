f855822438b8709fcb3a537fc03302ccf9869aca
c0ki <cokinou.web@gmail.com>
2015-11-11 18:13:46 +0100

    Update Admin

--- src/Core/CoreBundle/Component/Routing/Generator/UrlGenerator.php
+++ src/Core/CoreBundle/Component/Routing/Generator/UrlGenerator.php
@@ -13,14 +13,24 @@ class UrlGenerator extends ComponentUrlGenerator
     {
         if ($name{0} != '_' && (array_key_exists('_site', $parameters) || $this->getContext()->hasParameter('_site'))) {
             $site = null;
-            if ($this->getContext()->hasParameter('_site')) {
-                $site = $this->getContext()->getParameter('_site');
+            if (!empty($defaults) && array_key_exists('_site', $defaults)) {
+                $site = $defaults['_site'];
+                if (array_key_exists('_site', $parameters)) {
+                    unset($parameters['_site']);
+                }
             }
-            if (array_key_exists('_site', $parameters)) {
-                $site = $parameters['_site'];
-                unset($parameters['_site']);
+            else {
+                if ($this->getContext()->hasParameter('_site')) {
+                    $site = $this->getContext()->getParameter('_site');
+                }
+                if (array_key_exists('_site', $parameters)) {
+                    $site = $parameters['_site'];
+                    unset($parameters['_site']);
+                }
             }
-            array_push($tokens, array('text', "/{$site}"));
+            if (!empty($site)) {
+                array_push($tokens, array('text', "/{$site}"));
+            }
         }
 
         $url = parent::doGenerate($variables, $defaults, $requirements, $tokens, $parameters, $name, $referenceType, $hostTokens, $requiredSchemes);
