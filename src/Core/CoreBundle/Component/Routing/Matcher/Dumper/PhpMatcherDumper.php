586b0e0534a5dd5be47ccb46fe3b3c58a724610e
c0ki <cokinou.web@gmail.com>
2015-11-13 14:37:54 +0100

    Update matcher to generic usage

--- src/Core/CoreBundle/Component/Routing/Matcher/Dumper/PhpMatcherDumper.php
+++ src/Core/CoreBundle/Component/Routing/Matcher/Dumper/PhpMatcherDumper.php
@@ -1,25 +1,9 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Core\CoreBundle\Component\Routing\Matcher\Dumper;
 
 use Symfony\Component\Routing\Matcher\Dumper\PhpMatcherDumper as FrameworkPhpMatcherDumper;
 
-/**
- * PhpMatcherDumper creates a PHP class able to match URLs for a given set of routes.
- *
- * @author Fabien Potencier <fabien@symfony.com>
- * @author Tobias Schultze <http://tobion.de>
- * @author Arnaud Le Blanc <arnaud.lb@gmail.com>
- */
 class PhpMatcherDumper extends FrameworkPhpMatcherDumper
 {
     /**
@@ -27,18 +11,32 @@ class PhpMatcherDumper extends FrameworkPhpMatcherDumper
      */
     public function dump(array $options = array())
     {
+        foreach ($this->getRoutes()->all() as $name => $route) {
+            if (!$route->getRequirements()) {
+                continue;
+            }
+            $internalRequirementNames = preg_grep("/^_\w+/", array_keys($route->getRequirements()));
+            if (!$internalRequirementNames) {
+                continue;
+            }
 
-
-        foreach ($this->getRoutes()->all() as $route) {
-            if ($route->hasRequirement('_site')) {
-                $sites = explode('|', $route->getRequirement('_site'));
-                $requirements = $route->getRequirements();
-                unset($requirements['_site']);
-                $route->setRequirements($requirements);
+            foreach ($internalRequirementNames as $internalRequirementName) {
+                $internalRequirement = $route->getRequirement($internalRequirementName);
                 $condition = $route->getCondition() ? $route->getCondition() . ' and ' : '';
-                $route->setCondition($condition . "request.attributes.get('_site') in " . json_encode($sites));
+                if ($internalRequirement == '%') {
+                    $condition .= "request.attributes.has('{$internalRequirementName}')";
+                }
+                else {
+                    $internalRequirement = explode('|', $internalRequirement);
+                    $condition .= "request.attributes.get('{$internalRequirementName}') in " . json_encode($internalRequirement);
+                }
+                $requirements = $route->getRequirements();
+                unset($requirements[$internalRequirementName]);
+                $route->setRequirements($requirements);
+                $route->setCondition($condition);
             }
         }
+
         return parent::dump($options);
     }
 }
