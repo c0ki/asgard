f14874a8bf1c3019aad36eb1e3c32a517b72b93b
c0ki <cokinou.web@gmail.com>
2015-11-13 14:35:55 +0100

    Update global project manager

---
+++ src/Core/ProjectBundle/EventSubscriber/RouterSubscriber.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Core\ProjectBundle\EventSubscriber;
+
+use Core\ProjectBundle\Component\Helper\ProjectHelper;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouterInterface;
+
+class RouterSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var RequestContext
+     */
+    private $context;
+
+    /**
+     * @var ProjectHelper
+     */
+    protected $projectHelper;
+
+    /**
+     * @param ProjectHelper     $projectHelper
+     * @param RouterInterface     $matcher
+     * @param RequestContext|null $context
+     */
+    public function __construct(ProjectHelper $projectHelper, RouterInterface $matcher, RequestContext $context = null)
+    {
+        $this->projectHelper = $projectHelper;
+        $this->context = $context ?: $matcher->getContext();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        // return the subscribed events, their methods and priorities
+        return array(
+            'kernel.request' => array(
+                array('onKernelRequest', 99),
+            )
+        );
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        foreach ($this->projectHelper->listProjects() as $project) {
+            if (preg_match("#^/{$project->getName()}\b(.*)$#", $event->getRequest()->server->get('REQUEST_URI'), $matches)) {
+                $event->getRequest()->server->set('REQUEST_URI', $matches[1]);
+                $event->getRequest()->attributes->set('_project', $project->getName());
+                $this->context->setParameter('_project', $project->getName());
+                break;
+            }
+        }
+
+        foreach ($this->projectHelper->listEnvironments() as $environment) {
+            if (preg_match("#^/_{$environment->getName()}\b(.*)$#", $event->getRequest()->server->get('REQUEST_URI'), $matches)) {
+                $event->getRequest()->server->set('REQUEST_URI', $matches[1]);
+                $event->getRequest()->attributes->set('_environment', $environment->getName());
+                $this->context->setParameter('_environment', $environment->getName());
+                break;
+            }
+        }
+
+        return;
+    }
+}
