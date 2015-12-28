e7639701efdd0a93547bb4fc29609c2b4728193f
c0ki <cokinou.web@gmail.com>
2015-12-28 02:12:24 +0100

    Set cancel function

--- src/Core/LayoutBundle/Resources/public/js/init.js
+++ src/Core/LayoutBundle/Resources/public/js/init.js
@@ -1,6 +1,21 @@
-
 function needreload(event) {
     if (event.target.contentDocument == null) {
         event.target.src = event.target.dataset.srcerror;
     }
+}
+
+function cancel(e) {
+    // Stop propagation
+    e.preventDefault();
+
+    var node = e.target;
+    while (node.parentNode && node.parentNode.getAttribute) {
+        console.log(node.parentNode);
+        if (node.parentNode.getAttribute('id') == 'popin') {
+            popinClose(e);
+            return;
+        }
+        node = node.parentNode;
+    }
+    history.back();
 }
