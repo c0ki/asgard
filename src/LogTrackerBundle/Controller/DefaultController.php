5de64c5d418549d977108ba609844daaad90188f
c0ki <cokinou.web@gmail.com>
2016-02-04 15:57:39 +0100

    Update LogTracker bundle to manage dynamic parameters from route

--- src/LogTrackerBundle/Controller/DefaultController.php
+++ src/LogTrackerBundle/Controller/DefaultController.php
@@ -8,17 +8,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class DefaultController extends Controller
 {
     public function indexAction() {
-        return $this->render('LogTrackerBundle:Default:index.html.twig', array());
-    }
-
-    public function viewAction() {
-        return $this->render('LogTrackerBundle:Default:view.html.twig', array());
-    }
-
-    public function viewDataErrorAction() {
         $projectHelper = $this->container->get('project_helper');
         $indexer = $this->container->get('core.indexer.solr');
-        $data = array('dataset' => array(), 'schema' => array());
+        $data = array('daemon' => array(), 'type' => array(), 'daemontype' => array());
 
         $criteria = array();
         if ($projectHelper->hasProject()) {
@@ -27,29 +19,71 @@ class DefaultController extends Controller
         if ($projectHelper->hasDomain()) {
             $criteria['domain'] = $projectHelper->getDomain()->getName();
         }
-        $criteria[] = '+type:error';
         $criteria = array_filter($criteria);
-        $results = $indexer->search('asgard_logs', $criteria, 0, 1, array('daemon'));
-        foreach ($results->facets['daemon'] as $serverType => $val) {
-            $criteriaServerType = array_merge($criteria, array('+daemon' => $serverType));
-            $resultsServerType = $indexer->search('asgard_logs', $criteriaServerType, 0, 1, array('type_s'));
-            $firstDate = $resultsServerType->results[0]->date;
-            $firstDay = date('Y-m-d', strtotime($firstDate));
-            foreach ($resultsServerType->facets['type_s'] as $type => $val) {
-                $criteriaType = array_merge($criteriaServerType, array('+type_s' => $type));
-                $resultsType = $indexer->search('asgard_logs', $criteriaType, 0, 1,
-                    array('date' => array('date' => array('start' => $firstDay, 'gap' => '+1DAY'))));
-                foreach ($resultsType->facets['date'] as $date => $nb) {
-                    if (!array_key_exists($date, $data['dataset'])) {
-                        $data['dataset'][$date]["total"] = 0;
-                    }
-                    $data['schema']["{$serverType} / {$type}"] = $resultsType->query;
-                    $data['dataset'][$date]["date"] = substr($date, 0, 10);
-                    $data['dataset'][$date]["{$serverType} / {$type}"] = $nb;
-                    $data['dataset'][$date]["total"] += $nb;
+        $results = $indexer->search('asgard_logs', $criteria, 0, 1, array('daemon', 'type'));
+        $data['daemon'] = $results->facets['daemon'];
+        $data['type'] = $results->facets['type'];
+        foreach ($results->facets['daemon'] as $daemon => $val) {
+            $criteriaDaemon = array_merge($criteria, array('+daemon' => $daemon));
+            $resultsDaemon = $indexer->search('asgard_logs', $criteriaDaemon, 0, 1, array('type'));
+            $data['daemontype'][$daemon] = $resultsDaemon->facets['type'];
+        }
+
+        return $this->render('LogTrackerBundle:Default:index.html.twig', array('list' => $data));
+    }
+
+    public function viewAction($daemon, $type) {
+        $query = array();
+        $query['+daemon'] = $daemon;
+        $query['+type'] = $type;
+        $query = implode(' ', array_filter($query));
+        return $this->render('LogTrackerBundle:Default:view.html.twig', array('daemon' => $daemon, 'type' => $type, 'query' => $query));
+    }
+
+    public function viewDataAction($query, $preventMonth) {
+        $projectHelper = $this->container->get('project_helper');
+        $indexer = $this->container->get('core.indexer.solr');
+        $data = array('dataset' => array(), 'schema' => array());
+
+        $criteria = array($query);
+        if ($projectHelper->hasProject()) {
+            $criteria['+project'] = $projectHelper->getProject()->getName();
+        }
+        if ($projectHelper->hasDomain()) {
+            $criteria['+domain'] = $projectHelper->getDomain()->getName();
+        }
+        $criteria = array_filter($criteria);
+
+        $results = $indexer->search('asgard_logs', $criteria, 0, 1, array('subtype_s'));
+        if (empty($preventMonth) || !is_numeric($preventMonth)) {
+            $preventMonth = 6;
+        }
+        $firstDay = date('Y-m-d', strtotime("-{$preventMonth} MONTH"));
+        foreach ($results->facets['subtype_s'] as $name => $val) {
+            if ($name === '') {
+                $criteriaFacet = array_merge($criteria, array("-subtype_s" => '*'));
+                $name = "request";
+            }
+            else {
+                $criteriaFacet = array_merge($criteria, array("+subtype_s" => $name));
+            }
+            $resultsSubtype = $indexer->search('asgard_logs',
+                                            $criteriaFacet,
+                                            0,
+                                            1,
+                                            array('date' => array('date' => array('start' => $firstDay,
+                                                                                  'gap' => '+1DAY'))));
+            foreach ($resultsSubtype->facets['date'] as $date => $nb) {
+                if (!array_key_exists($date, $data['dataset'])) {
+                    $data['dataset'][$date]["total"] = 0;
                 }
+                $data['schema'][$name] = $resultsSubtype->query;
+                $data['dataset'][$date]["date"] = substr($date, 0, 10);
+                $data['dataset'][$date][$name] = $nb;
+                $data['dataset'][$date]["total"] += $nb;
             }
         }
+
         ksort($data['dataset']);
         $data['dataset'] = array_values($data['dataset']);
 
@@ -63,13 +97,13 @@ class DefaultController extends Controller
 
         $indexer = $this->container->get('core.indexer.solr');
         $results = $indexer->search('asgard_logs',
-            $query,
-            $start,
-            $rows,
-            array('daemon',
-                  'type_s',
-                  'date' => array('date' => array('gap' => '+1DAY'))));
+                                    $query,
+                                    $start,
+                                    $rows,
+                                    array('daemon',
+                                          'type_s',
+                                          'date' => array('date' => array('gap' => '+1DAY'))));
 
-        return $this->render('LogTrackerBundle:Default:results.html.twig', array('results' => $results));
+        return $this->render('LogTrackerBundle:List:results.html.twig', array('results' => $results));
     }
 }
