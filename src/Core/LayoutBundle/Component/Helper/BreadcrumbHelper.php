5171eef8d18deb849c260352401d7974bc1c3d55
c0ki <cokinou.web@gmail.com>
2016-02-13 16:04:57 +0100

    Adjust breadcrumb

--- src/Core/LayoutBundle/Component/Helper/BreadcrumbHelper.php
+++ src/Core/LayoutBundle/Component/Helper/BreadcrumbHelper.php
@@ -11,7 +11,7 @@ class BreadcrumbHelper
     /**
      * @var \Symfony\Component\HttpFoundation\Request
      */
-    protected $request = null;
+    protected $masterRequest = null;
 
     /**
      * @var ToolHelper
@@ -25,19 +25,25 @@ class BreadcrumbHelper
 
     public function __construct(RequestStack $requestStack, ToolHelper $toolHelper, $template)
     {
-        $this->request = $requestStack->getMasterRequest();
+        $this->masterRequest = $requestStack->getMasterRequest();
         $this->toolHelper = $toolHelper;
         $this->template = $template;
     }
 
     public function getBreadcrumbData(array $params = array()) {
+        $currentTitle = 'Default';
+        if (preg_match('/::(\w+)Action$/', $this->masterRequest->attributes->get('_controller'), $matches)) {
+            $currentTitle = $matches[1];
+        }
+
         $routes = array();
-        $routes['current'] = array(
-            'route'  => $this->request->attributes->get('_route'),
-            'params' => $this->request->attributes->get('_route_params'),
+        $routes[$currentTitle] = array(
+            'route'  => $this->masterRequest->attributes->get('_route'),
+            'params' => $this->masterRequest->attributes->get('_route_params'),
         );
 
         $routes['tool'] = $this->toolHelper->getTool();
+        $routes['root'] = array('label' => 'home', 'logo' => 'home', 'route' => 'core_layout_root');
 
         $routes = array_filter($routes);
 
--- src/Core/ProjectBundle/Component/Helper/BreadcrumbHelper.php
+++ src/Core/ProjectBundle/Component/Helper/BreadcrumbHelper.php
@@ -11,31 +11,36 @@ class BreadcrumbHelper extends CoreBreadcrumbHelper
      */
     protected $projectHelper;
 
-    public function setProjectHelper(ProjectHelper $projectHelper)
-    {
+    public function setProjectHelper(ProjectHelper $projectHelper) {
         $this->projectHelper = $projectHelper;
     }
 
     public function getBreadcrumbData(array $params = array()) {
         $routes = parent::getBreadcrumbData($params);
+        $rootRoute = array_shift($routes);
         if ($this->projectHelper->hasDomain()) {
             $domain = $this->projectHelper->getDomain();
             $element = array(
-                'route' => 'core_project_domain',
+                'route'  => 'core_project_domain',
                 'params' => array('@domain' => $domain->getName()),
-                'label' => $domain,
+                'label'  => $domain,
+                'logo'   => $domain->getLogo(),
             );
             array_unshift($routes, $element);
         }
         if ($this->projectHelper->hasProject()) {
             $project = $this->projectHelper->getProject();
             $element = array(
-                'route' => 'core_project_project',
+                'route'  => 'core_project_project',
                 'params' => array('@project' => $project->getName()),
-                'label' => $project,
+                'label'  => $project,
+                'logo'   => $project->getLogo(),
             );
             array_unshift($routes, $element);
         }
+        $rootRoute['params'] = array('@project' => null, '@domain' => null);
+        array_unshift($routes, $rootRoute);
+
         return $routes;
     }
 
