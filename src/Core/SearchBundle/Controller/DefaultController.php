2c0b24c40f0b687844af139dccb308dcc1b9d1b4
c0ki <cokinou.web@gmail.com>
2016-02-16 16:24:58 +0100

    Add render to search form

--- src/Core/SearchBundle/Controller/DefaultController.php
+++ src/Core/SearchBundle/Controller/DefaultController.php
@@ -2,12 +2,38 @@
 
 namespace Core\SearchBundle\Controller;
 
+use Core\CoreBundle\Component\Indexer\SolrQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('CoreSearchBundle:Default:index.html.twig', array('name' => $name));
+    public function formAction($query = null) {
+        $form = $this->createForm('core_search', array('query' => $query));
+
+        return $this->render('CoreSearchBundle:Default:form.html.twig',
+            array('form' => $form->createView(), 'query' => $query));
+    }
+
+    public function submitAction(Request $request) {
+        $query = null;
+
+        // Form init
+        $form = $this->createForm('core_search');
+        $form->handleRequest($request);
+
+        // Redirect to valid url if form valid
+        if ($form->isValid()) {
+            $query = $form->get('query')->getData();
+            SolrQuery::formatCriteria($query);
+            parse_str($form->get('_route_params')->getData(), $routeParams);
+            $routeParams = array_merge($routeParams, array('query' => $query));
+            return new RedirectResponse($this->generateUrl($form->get('_route')->getData(), $routeParams));
+        }
+        $this->addFlash('error', "Invalid search query");
+        $url = $this->container->get('request_stack')->getMasterRequest()->headers->get('referer');
+
+        return new RedirectResponse($url);
     }
 }
--- src/Core/SearchBundle/Form/Type/SearchType.php
+++ src/Core/SearchBundle/Form/Type/SearchType.php
@@ -4,19 +4,36 @@ namespace Core\SearchBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Router;
 
 class SearchType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('query')
-            ->add('search', 'submit')
-        ;
+    /**
+     * @var Request
+     */
+    protected $masterRequest;
+
+    /**
+     * @var Router
+     */
+    protected $router;
+
+    public function __construct(RequestStack $requestStack, Router $router) {
+        $this->masterRequest = $requestStack->getMasterRequest();
+        $this->router = $router;
     }
 
-    public function getName()
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->setAction($this->router->generate('core_search_submit'))
+            ->add('query')
+            ->add('_route', 'hidden', array('data' => $this->masterRequest->attributes->get('_route')))
+            ->add('_route_params', 'hidden', array('data' => http_build_query($this->masterRequest->attributes->get('_route_params'))));
+    }
+
+    public function getName() {
         return 'core_search';
     }
 }
