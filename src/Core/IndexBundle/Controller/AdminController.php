0cf200295f68629f44548d65f5f0fac92dca7d5e
c0ki <cokinou.web@gmail.com>
2016-02-17 15:52:34 +0100

    Add empty admin controller

---
+++ src/Core/IndexBundle/Controller/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\IndexBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    public function removeDataAction()
+    {
+        var_dump('TODO');
+    }
+}
